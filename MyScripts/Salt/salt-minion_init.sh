1f9acf3f2901026f03814ec8ea8c6258033604de
vzlatkov <vzlatkov.vzlatkov@xi-group.com>
2021-06-01 16:41:00 +0300

    Init Script For Salt Minion v1.0.1

---
+++ MyScripts/Salt/salt-minion_init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+###This is a script for automatization of the installation and configuration of a Salt Stack minion server. By VZlatkov (a.k.a. Xerxes)
+###v1.0.1
+### In this version we are going to set the basic installation of the Salt-minion by updating and upgrading the OS and installing the needed dependencies. Then we are going to install the minion server itself.
+
+
+###This is the initial dependency installation part of the script
+apt -y update
+apt -y upgrade
+apt -y install python3
+apt -y install python3-pip
+pip install salt #You first install the Salt Stack, then you configure it as a Master or Slave
+# Download key
+curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest/salt-archive-keyring.gpg
+# Create apt sources list file
+echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest focal main" | sudo tee /etc/apt/sources.list.d/salt.list
+apt-get update
+apt-get -y install salt-master
+
+###Start the service
+systemctl daemon-reload
+systemctl start salt-minion
+systemctl restart salt-minion
+systemctl status salt-minion
+
+
