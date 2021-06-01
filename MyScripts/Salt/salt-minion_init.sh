1b6bb58722f8b88886063b5acf2df6fb3f54505a
vzlatkov <vzlatkov.vzlatkov@xi-group.com>
2021-06-01 17:05:53 +0300

    Init Script For Salt Minion v1.0.2

--- MyScripts/Salt/salt-minion_init.sh
+++ MyScripts/Salt/salt-minion_init.sh
@@ -3,6 +3,11 @@
 ###This is a script for automatization of the installation and configuration of a Salt Stack minion server. By VZlatkov (a.k.a. Xerxes)
 ###v1.0.1
 ### In this version we are going to set the basic installation of the Salt-minion by updating and upgrading the OS and installing the needed dependencies. Then we are going to install the minion server itself.
+###v1.0.2
+###Since this script is intended to be used as a user data and can not be interactive, this revision is going to make a new interactive script on the machine/EC2 cloud instance that will remain there for further configuration (Connecting to the master, etc.)
+
+
+
 
 
 ###This is the initial dependency installation part of the script
@@ -24,4 +29,22 @@ systemctl start salt-minion
 systemctl restart salt-minion
 systemctl status salt-minion
 
+			#####			V.1.0.2   STARTS FROM HERE			#####
 
+
+###This is a heredoc that would create the new bash script on the instance
+(
+cat > $HOME/salt_minion_conf.sh<<EOF
+
+read -p "insert the master's hostname `\n`" name
+read -p "insert the master's finger `\n`" finger
+read -p "insert the master's IP `\n`" mIP
+echo "master: $name" >> /etc/salt/minion
+echo "master_finger: '$finger'" >> /etc/salt/minion
+
+###Add the master in the /etc/hosts file in order to be able to resolve the master server's name
+echo "$mIP $name" >> /etc/hosts
+EOF
+)
+###Make the file Executable
+chmod +x $HOME/salt_minion_conf.sh
